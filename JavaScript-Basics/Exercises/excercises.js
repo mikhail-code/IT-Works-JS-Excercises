9aaf2f3e00098f20ada6595b8180f91358836467
Mikhail Kravchenko <km.code@proton.me>
2024-03-13 00:58:33 +0200

    ex 6

--- JavaScript-Basics/Exercises/excercises.js
+++ JavaScript-Basics/Exercises/excercises.js
@@ -116,3 +116,36 @@ function getCurrentDate() {
     let dateString = `Today is ${day} the ${date} of ${month} ${year}`;
     return dateString;
 }
+
+//ex 6
+// From function declarations to function expressions
+const welcome = function() {
+    let welcome = 'Welcome to Appleseeds Bootcamp!';
+    return welcome;
+};
+
+const power = function(a) {
+    let myNumber = a;
+    let result = Math.pow(myNumber, 2);
+    return result;
+};
+
+const add = function(a, b = 5) {
+    let myNumber = a;
+    let sum = myNumber + b;
+    return sum;
+};
+
+// From function expressions to function declarations
+function hello() {
+    return "Hello!";
+}
+
+function squareRoot(a) {
+    return Math.sqrt(a);
+}
+
+function randomNumbers(a, b) {
+    return Math.random() * (a - b) + b;
+}
+
