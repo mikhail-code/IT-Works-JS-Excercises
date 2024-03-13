4d2c093e3a16f6685f756fc86f76e9b07840780d
Mikhail Kravchenko <km.code@proton.me>
2024-03-13 10:07:52 +0200

    ex 12

--- JavaScript-Basics/Exercises/excercises.js
+++ JavaScript-Basics/Exercises/excercises.js
@@ -276,3 +276,16 @@ if (johnAverage > mikeAverage && johnAverage > maryAverage) {
 } else {
     console.log(`It's a draw!`);
 }
+
+//ex 12
+function printBoomNumbers(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 7 === 0 && String(i).includes('7')) {
+            console.log("BOOM-BOOM");
+        } else if (i % 7 === 0) {
+            console.log("BOOM");
+        } else {
+            console.log(i);
+        }
+    }
+}
