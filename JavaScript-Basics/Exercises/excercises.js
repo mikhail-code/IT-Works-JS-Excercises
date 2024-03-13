44dbfdf09fa6034d418d9dabdd0de99a074739ca
Mikhail Kravchenko <km.code@proton.me>
2024-03-13 09:57:50 +0200

    ex 10

--- JavaScript-Basics/Exercises/excercises.js
+++ JavaScript-Basics/Exercises/excercises.js
@@ -213,3 +213,18 @@ if (numSiblings == 1) {
 }
 
 numSiblings = Number(numSiblings);
+
+//ex 10
+function getGrade(score) {
+    if (score >= 90 && score <= 100) {
+        return "A";
+    } else if (score >= 80 && score < 90) {
+        return "B";
+    } else if (score >= 70 && score < 80) {
+        return "c";
+    } else if (score >= 65 && score < 70) {
+        return "D";
+    } else if (score >= 0 && score < 65) {
+        return "F";
+    }
+}
