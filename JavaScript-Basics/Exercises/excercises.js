09af59caf37207eb8f640a9a90c1a6caf031d135
Mikhail Kravchenko <km.code@proton.me>
2024-03-13 09:50:21 +0200

    ex 9

--- JavaScript-Basics/Exercises/excercises.js
+++ JavaScript-Basics/Exercises/excercises.js
@@ -199,3 +199,17 @@ let usaPercentage2 = percentageOfWorld2(331);
 console.log(`Russia: ${russiaPercentage2}%`);
 console.log(`Israel: ${israelPercentage2}%`);
 console.log(`USA: ${usaPercentage2}%`);
+
+
+//ex 9
+let numSiblings = prompt('How many siblings do you have?');
+
+if (numSiblings == 1) {
+    console.log('1 sibling');
+} else if (numSiblings > 1) {
+    console.log('More than 1 sibling');
+} else {
+    console.log('No siblings');
+}
+
+numSiblings = Number(numSiblings);
